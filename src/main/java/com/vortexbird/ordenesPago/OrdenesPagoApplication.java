82ee539dc809a4f54d10b83598423a98e230499e
pancha93 <alarroyavec_1@uqvirtual.edu.co>
2026-03-04 16:25:53 -0500

    esqueleto inicial

---
+++ src/main/java/com/vortexbird/ordenesPago/OrdenesPagoApplication.java
@@ -0,0 +1,13 @@
+package com.vortexbird.ordenesPago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenesPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenesPagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vortexbird/ordenesPago/OrdenesPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexbird.ordenesPago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenesPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
